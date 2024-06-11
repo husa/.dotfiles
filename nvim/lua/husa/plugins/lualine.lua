f20987b6900d6b6792c25a84e50fe50e61909c57
husa <1536549+husa@users.noreply.github.com>
2024-06-11 23:11:27 +0300

    add lualine

---
+++ nvim/lua/husa/plugins/lualine.lua
@@ -0,0 +1,15 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  event = "VeryLazy",
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function() 
+    local lualine = require('lualine')
+
+    lualine.setup({
+      options = {
+        theme = 'gruvbox',
+        globalstatus = false 
+      }
+    })
+  end
+}
