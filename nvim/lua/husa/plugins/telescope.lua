d12496ae5c0be405821d329d90bd77291d54aac7
husa <1536549+husa@users.noreply.github.com>
2024-06-11 23:11:40 +0300

    add telescope(very basic)

--- nvim/lua/husa/plugins/telescope.lua
+++ nvim/lua/husa/plugins/telescope.lua
@@ -1,1 +1,11 @@
-return {}
+return {
+  'nvim-telescope/telescope.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  keys = {
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+    { "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Find old files" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Find string" },
+    { "<leader>fc", "<cmd>Telescope grep_string<cr>", desc = "Find string under cursor" },
+    { "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Find todos" }
+  }
+}
