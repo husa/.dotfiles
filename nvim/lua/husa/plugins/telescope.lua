7f1262885fbb9c58ebf834abf5455230e07476f0
husa <1536549+husa@users.noreply.github.com>
2024-06-11 23:42:55 +0300

    add telescope find buffers command

--- nvim/lua/husa/plugins/telescope.lua
+++ nvim/lua/husa/plugins/telescope.lua
@@ -5,6 +5,7 @@ return {
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
     { "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Find old files" },
     { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Find string" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
     { "<leader>fc", "<cmd>Telescope grep_string<cr>", desc = "Find string under cursor" },
     { "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Find todos" }
   }
