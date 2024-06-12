5500b5025cea0adc9bea021b6dcb2dab0ebee36f
husa <1536549+husa@users.noreply.github.com>
2024-06-12 12:17:19 +0300

    add yanky, todo-comments, indent keymaps

--- nvim/lua/husa/core/keymaps.lua
+++ nvim/lua/husa/core/keymaps.lua
@@ -4,3 +4,6 @@ vim.keymap.set("n", "<leader>qa", "<cmd>qa<cr>", { desc = "Quit All" })
 vim.keymap.set("n", "<leader>qq", "<cmd>q<cr>", { desc = "Quit" })
 vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save" })
 
+-- better indent
+vim.keymap.set('v', '>', '>gv')
+vim.keymap.set('v', '<', '<gv')
