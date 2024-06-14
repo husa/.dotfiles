2d0bb66056da914a5522c47adb7a9412d68dc081
husa <1536549+husa@users.noreply.github.com>
2024-06-14 18:47:24 +0300

    ignore package-lock.json during search

--- nvim/lua/husa/plugins/telescope.lua
+++ nvim/lua/husa/plugins/telescope.lua
@@ -1,12 +1,19 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
     { "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Find old files" },
     { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Find string" },
     { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find buffers" },
     { "<leader>fc", "<cmd>Telescope grep_string<cr>", desc = "Find string under cursor" },
-    { "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Find todos" }
-  }
+    { "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Find todos" },
+  },
+  opts = {
+    defaults = {
+      file_ignore_patterns = {
+        "package%-lock%.json",
+      },
+    },
+  },
 }
