01b9c06f28b7a41744b58d65fa078a3ba01d7396
husa <1536549+husa@users.noreply.github.com>
2024-06-21 12:23:02 +0300

    add move lines keymaps

--- nvim/lua/husa/core/keymaps.lua
+++ nvim/lua/husa/core/keymaps.lua
@@ -23,3 +23,17 @@ map("n", "<leader>um", "<cmd>Mason<cr>", { desc = "Mason" })
 
 -- save file
 map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+-- Move Lines
+map("n", "<A-j>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+map("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+map("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
+map("n", "<A-Down>", "<cmd>m .+1<cr>==", { desc = "Move Down" })
+map("n", "<A-Up>", "<cmd>m .-2<cr>==", { desc = "Move Up" })
+map("i", "<A-Down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map("i", "<A-Up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map("v", "<A-Down>", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+map("v", "<A-Up>", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
