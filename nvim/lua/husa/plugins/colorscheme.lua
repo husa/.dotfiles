98fd3d624e911fb3d92b093fc13270ac55942067
husa <1536549+husa@users.noreply.github.com>
2024-07-08 13:33:43 +0300

    update colorsheme

--- nvim/lua/husa/plugins/colorscheme.lua
+++ nvim/lua/husa/plugins/colorscheme.lua
@@ -1,7 +1,63 @@
 return {
-  "ellisonleao/gruvbox.nvim",
-  priority = 1000,
-  config = function()
-    vim.cmd('colorscheme gruvbox')
-  end
+  -- gruvbox
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = true,
+    -- priority = 1000,
+  },
+  -- tokyonight
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+
+  -- catppuccin
+  {
+    "catppuccin/nvim",
+    -- lazy = true,
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
+    },
+    priority = 1000,
+    config = function(opts)
+      require("catppuccin").setup(opts)
+      vim.cmd("colorscheme catppuccin")
+    end,
+  },
 }
--- nvim/lua/husa/plugins/lualine.lua
+++ nvim/lua/husa/plugins/lualine.lua
@@ -7,7 +7,6 @@ return {
 
     lualine.setup({
       options = {
-        theme = "gruvbox",
         globalstatus = false,
       },
     })
