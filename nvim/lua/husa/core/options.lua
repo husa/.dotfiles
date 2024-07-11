718372f007721c9bb32e115f88eaa405c4a79953
husa <1536549+husa@users.noreply.github.com>
2024-07-11 12:26:08 +0300

    wrap by word

--- nvim/lua/husa/core/options.lua
+++ nvim/lua/husa/core/options.lua
@@ -23,7 +23,9 @@ opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
+-- wrapping
 opt.wrap = true
+opt.linebreak = true
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
