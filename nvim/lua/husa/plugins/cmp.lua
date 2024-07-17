8af11841d4deb5096b9cc0f9c446c454063b7bfd
husa <1536549+husa@users.noreply.github.com>
2024-07-17 18:31:35 +0300

    (nvim) accept completion with tab

--- nvim/lua/husa/plugins/cmp.lua
+++ nvim/lua/husa/plugins/cmp.lua
@@ -20,6 +20,11 @@ return {
   config = function()
     local cmp = require("cmp")
 
+    local has_words_before = function()
+      unpack = unpack or table.unpack
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
     -- local luasnip = require("luasnip")
 
     local lspkind = require("lspkind")
@@ -44,6 +49,15 @@ return {
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
         ["<C-e>"] = cmp.mapping.abort(), -- close completion window
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<Tab>"] = function(fallback)
+          if not cmp.select_next_item() then
+            if vim.bo.buftype ~= "prompt" and has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end
+        end,
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
