8d68adfbf09b1590d468140f173e0006d08a7078
husa <1536549+husa@users.noreply.github.com>
2024-07-26 16:35:55 +0300

    add complete by tab

--- nvim/lua/husa/plugins/cmp.lua
+++ nvim/lua/husa/plugins/cmp.lua
@@ -20,11 +20,12 @@ return {
   config = function()
     local cmp = require("cmp")
 
-    local has_words_before = function()
-      unpack = unpack or table.unpack
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
+    -- local has_words_before = function()
+    --   unpack = unpack or table.unpack
+    --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    -- end
+
     -- local luasnip = require("luasnip")
 
     local lspkind = require("lspkind")
@@ -49,15 +50,27 @@ return {
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
         ["<C-e>"] = cmp.mapping.abort(), -- close completion window
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
-        ["<Tab>"] = function(fallback)
-          if not cmp.select_next_item() then
-            if vim.bo.buftype ~= "prompt" and has_words_before() then
-              cmp.complete()
-            else
-              fallback()
+        -- ["<Tab>"] = function(fallback)
+        --   if not cmp.select_next_item() then
+        --     if vim.bo.buftype ~= "prompt" and has_words_before() then
+        --       cmp.complete()
+        --     else
+        --       fallback()
+        --     end
+        --   end
+        -- end,
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
+          if cmp.visible() then
+            local entry = cmp.get_selected_entry()
+            if not entry then
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
             end
+            cmp.confirm()
+          else
+            fallback()
           end
-        end,
+        end, { "i" }),
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
