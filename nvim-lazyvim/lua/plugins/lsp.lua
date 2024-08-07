8c3942cfa32979dcdcd33f27bae1ce75ae950caf
husa <1536549+husa@users.noreply.github.com>
2024-08-07 18:04:50 +0300

    (nvim) disable inlay hints by default

--- nvim-lazyvim/lua/plugins/lsp.lua
+++ nvim-lazyvim/lua/plugins/lsp.lua
@@ -2,6 +2,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      inlay_hints = { enabled = false }, -- disable inlay_hints by default
       servers = {
         cssls = {},
         somesass_ls = {},
