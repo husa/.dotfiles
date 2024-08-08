60b4a998247e8e48da47116ea924740124b2f124
husa <1536549+husa@users.noreply.github.com>
2024-08-08 13:20:11 +0300

    (nvim) add telescope resume and filetypes keymaps

--- nvim-lazyvim/lua/plugins/telescope.lua
+++ nvim-lazyvim/lua/plugins/telescope.lua
@@ -2,5 +2,8 @@ return {
   "nvim-telescope/telescope.nvim",
   keys = {
     { "<leader>fr", "<cmd>Telescope oldfiles cwd_only=true<cr>", desc = "Recent" },
+    { "<leader>fl", "<cmd>Telescope resume<cr>", desc = "Resume last search " },
+
+    { "<leader>ut", "<cmd>Telescope filetypes<cr>", desc = "Filetype" },
   },
 }
