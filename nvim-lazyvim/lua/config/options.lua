8f46088c5f2aa94fe9a466994174f5e118437c24
husa <1536549+husa@users.noreply.github.com>
2024-08-08 13:19:30 +0300

    (nvim) add filetype detection for k8s config and git config

--- nvim-lazyvim/lua/config/options.lua
+++ nvim-lazyvim/lua/config/options.lua
@@ -1,7 +1,3 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
 -- NeoVide
 if vim.g.neovide then
   -- opt.cmdheight = 0
@@ -9,7 +5,17 @@ end
 vim.g.neovide_window_blurred = true
 vim.g.neovide_transparency = 0.85
 
+-- Regular options
 local opt = vim.opt
 
 -- wrapping
 opt.wrap = true -- wrap by default
+
+-- Custom filetypes
+vim.filetype.add({
+  filename = {},
+  pattern = {
+    [".*/git/config"] = "gitconfig",
+    [".*/.kube/config"] = "yaml",
+  },
+})
