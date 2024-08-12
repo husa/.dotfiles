906cd3ee36831a45b33d182135bd8319b16115ba
husa <1536549+husa@users.noreply.github.com>
2024-08-12 12:03:22 +0300

    (nvim) add emoji for markdown

--- nvim-lazyvim/lua/plugins/nvim-cmp.lua
+++ nvim-lazyvim/lua/plugins/nvim-cmp.lua
@@ -1,15 +1,29 @@
 return {
   "hrsh7th/nvim-cmp",
+  dependencies = { "hrsh7th/cmp-emoji" },
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
-    local has_words_before = function()
-      unpack = unpack or table.unpack
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
+    -- local has_words_before = function()
+    --   unpack = unpack or table.unpack
+    --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    --   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    -- end
+
+    -- add Emoji support
+    -- table.insert(opts.sources, { name = "emoji" })
 
     local cmp = require("cmp")
 
+    -- add emoji support only in markdown
+    cmp.setup.filetype({ "markdown" }, {
+      sources = {
+        { name = "buffer" },
+        { name = "path" },
+        { name = "emoji" },
+      },
+    })
+
+    -- complete with Tab
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
       ["<Tab>"] = cmp.mapping(function(fallback)
         -- This little snippet will confirm with tab, and if no entry is selected, will confirm the first item
