458053282535ead5bf2e1bce8fb38b3934163d98
husa <1536549+husa@users.noreply.github.com>
2024-08-20 11:59:16 +0300

    (nvim) add toml lsp + schema for starship config

--- nvim-lazyvim/lua/config/lazy.lua
+++ nvim-lazyvim/lua/config/lazy.lua
@@ -26,6 +26,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
     -- experiments
     { import = "lazyvim.plugins.extras.coding.codeium" },
     -- import/override with your plugins
