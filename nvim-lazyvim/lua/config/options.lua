7c30b0fa9c967e961e1e08f9972f9d24e548807f
husa <1536549+husa@users.noreply.github.com>
2024-09-04 14:58:29 +0300

    (nvim) increase linespacing in neovide

--- nvim-lazyvim/lua/config/options.lua
+++ nvim-lazyvim/lua/config/options.lua
@@ -1,12 +1,15 @@
+local opt = vim.opt
+
 -- NeoVide
 if vim.g.neovide then
   -- opt.cmdheight = 0
+  opt.linespace = 2
 end
+
 vim.g.neovide_window_blurred = true
 vim.g.neovide_transparency = 0.85
 
 -- Regular options
-local opt = vim.opt
 
 -- wrapping
 opt.wrap = true -- wrap by default
