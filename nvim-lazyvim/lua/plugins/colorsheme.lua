385ab933f3c857997b0544760c4e6863cadef6b7
husa <1536549+husa@users.noreply.github.com>
2024-09-07 14:36:27 +0300

    (nvim) enable transparent background, with fix for neovide

--- nvim-lazyvim/lua/plugins/colorsheme.lua
+++ nvim-lazyvim/lua/plugins/colorsheme.lua
@@ -1,6 +1,15 @@
 return {
-  { "catppuccin/nvim" },
-
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    opts = function(_, opts)
+      -- enable transparent background, but not in neovide
+      if not vim.g.neovide then
+        opts.transparent_background = true
+      end
+      return opts
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
