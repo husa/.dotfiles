b01680aab97e0952e29356f89f1575cb03553474
husa <1536549+husa@users.noreply.github.com>
2024-09-11 16:21:03 +0300

    (nvim) disable root detection by LazyVim

--- nvim-lazyvim/lua/config/options.lua
+++ nvim-lazyvim/lua/config/options.lua
@@ -9,6 +9,11 @@ end
 vim.g.neovide_window_blurred = true
 vim.g.neovide_transparency = 0.85
 
+-- LazyVim
+-- disable root detection(not convenient when editing multi-language dirs, like this ".dotfiles" one)
+-- default is vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
+vim.g.root_spec = { "cwd" }
+
 -- Regular options
 
 -- wrapping
