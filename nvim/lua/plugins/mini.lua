3d38d6a79b9252e36bc0dab4833e5a7df6db3c9e
husa <1536549+husa@users.noreply.github.com>
2024-12-13 14:03:16 +0200

    (nvim) set mini.cursorword highlight groups

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -10,6 +10,11 @@ return {
     require("mini.indentscope").setup()
     require("mini.pairs").setup()
     require("mini.surround").setup()
+
+    -- set mini.cursorword highlight groups(bg color without underline)
+    local colors = require("catppuccin.palettes").get_palette("mocha")
+    vim.api.nvim_set_hl(0, "MiniCursorword", { bg = colors.surface1 })
+    vim.api.nvim_set_hl(0, "MiniCursorwordCurrent", { underline = false })
   end,
   keys = {
     { "<leader>bd", "<cmd>:lua MiniBufremove.delete()<cr>", mode = "n", desc = "Delete Buffer" },
