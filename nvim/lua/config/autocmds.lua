cde395f527535b81e4d34da61cd9d296112b8961
husa <1536549+husa@users.noreply.github.com>
2024-12-16 18:22:35 +0200

    (nvim) add auto-delete of empty buffers

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,9 +1,10 @@
+-- Toggle relative numbers
 -- from https://github.com/sitiom/nvim-numbertoggle/tree/main
-local augroup = vim.api.nvim_create_augroup("numbertoggle", {})
+local number_toggle_augroup = vim.api.nvim_create_augroup("numbertoggle", {})
 
 vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
   pattern = "*",
-  group = augroup,
+  group = number_toggle_augroup,
   callback = function()
     if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
       vim.opt.relativenumber = true
@@ -13,11 +14,34 @@ vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "Cmdline
 
 vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
   pattern = "*",
-  group = augroup,
+  group = number_toggle_augroup,
   callback = function()
     if vim.o.nu then
       vim.opt.relativenumber = false
       vim.cmd("redraw")
+    end
+  end,
+})
+
+local empty_buf_delete_augroup = vim.api.nvim_create_augroup("emptybufdelete", {})
+vim.api.nvim_create_autocmd({ "BufLeave" }, {
+  pattern = "*",
+  group = empty_buf_delete_augroup,
+  callback = function()
+    local buffers = vim.api.nvim_list_bufs()
+    for _, buf in ipairs(buffers) do
+      if
+        vim.api.nvim_buf_is_loaded(buf)
+        and vim.api.nvim_buf_get_name(buf) == ""
+        and vim.api.nvim_buf_get_option(buf, "buftype") == ""
+        and vim.api.nvim_buf_get_option(buf, "buflisted")
+      then
+        local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+        if #lines == 1 and #lines[1] == 0 then
+          vim.api.nvim_buf_delete(buf, {})
+          print("deleted empty bufffer")
+        end
+      end
     end
   end,
 })
