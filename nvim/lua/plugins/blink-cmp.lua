f5170c41a8e4c240b491ae515815e3117d038f0c
husa <1536549+husa@users.noreply.github.com>
2024-12-17 15:52:12 +0200

    (nvim) use cr to accept completion

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -13,7 +13,7 @@ return {
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- see the "default configuration" section below for full documentation on how to define
     -- your own keymap.
-    keymap = { preset = "super-tab" },
+    keymap = { preset = "super-tab", ["<CR>"] = { "accept", "fallback" } },
 
     appearance = {
       use_nvim_cmp_as_default = false,
