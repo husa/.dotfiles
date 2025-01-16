96134fe7f5ff867b479c42869c04aea3bb576e4f
husa <1536549+husa@users.noreply.github.com>
2025-01-16 18:18:27 +0200

    (nvim) telescope: replace duplicate keybindings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -14,7 +14,7 @@ return {
     },
     -- find file
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File" },
-    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Find String" },
+    { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Find Git Files" },
     { "<leader>fo", "<cmd>Telescope oldfiles cwd_only=true<cr>", desc = "Find Old Files" },
     { "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Find Buffers" },
     { "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Find Todos" },
