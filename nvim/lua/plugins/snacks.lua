73197cb84d33da84eeb2e8ef176689278b19d71b
husa <1536549+husa@users.noreply.github.com>
2025-02-03 12:16:34 +0200

    (nvim) switch from neo-tree to snacks.explorer
    update all plugins

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,6 +1,6 @@
 return {
   "folke/snacks.nvim",
-  version = "*",
+  -- version = "*",
   -- priority = 1000,
   lazy = false,
   ---@type snacks.Config
@@ -15,11 +15,28 @@ return {
     -- scroll = { enabled = true },
     statuscolumn = { enabled = true },
     -- words = { enabled = true },
+    explorer = {
+      replace_netrw = false, -- Replace netrw with the snacks explorer
+    },
+    picker = {
+      sources = {
+        explorer = {
+          hidden = true,
+          ignored = true,
+          exclude = { ".DS_Store" },
+        },
+      },
+    },
   },
   keys = {
+    -- buf delete
     { "<leader>bd", "<cmd>:lua Snacks.bufdelete()<cr>", mode = "n", desc = "Delete Buffer" },
     { "<leader>bo", "<cmd>:lua Snacks.bufdelete.other()<cr>", mode = "n", desc = "Delete Other Buffers" },
     { "<leader>ba", "<cmd>:lua Snacks.bufdelete.all()<cr>", mode = "n", desc = "Delete All Buffers" },
     { "<leader>bD", "<cmd>:bdelete<cr>", mode = "n", desc = "Delete Buffer and Window" },
+
+    -- explorer
+    { "\\", "<cmd>:lua Snacks.explorer.open()<cr>", mode = "n", desc = "Explorer" },
+    { "|", "<cmd>:lua Snacks.explorer.open({focus = 'input'})<cr>", mode = "n", desc = "Explorer (Find)" },
   },
 }
