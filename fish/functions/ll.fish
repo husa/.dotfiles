65dd344d34f942c01acf25f1b46c33670fbeac00
husa <1536549+husa@users.noreply.github.com>
2025-02-17 13:56:39 +0200

    (fish) show git status in eza alias

--- fish/functions/ll.fish
+++ fish/functions/ll.fish
@@ -1,3 +1,3 @@
 function ll --wraps=ls --wraps='eza --all --group-directories-first --long --classify=always --icons=always --hyperlink --header --no-permissions --no-user --color --color-scale' --description 'list directory'
-    eza --all --group-directories-first --long --classify=always --icons=always --hyperlink --header --no-permissions --no-user --color --color-scale $argv
+    eza --all --group-directories-first --long --classify=always --icons=always --hyperlink --header --no-permissions --no-user --color --color-scale --git $argv
 end
