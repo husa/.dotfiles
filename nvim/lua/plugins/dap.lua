1c04d9807da5cf4bf558f10986e577faad231d65
husa <1536549+husa@users.noreply.github.com>
2025-03-26 15:03:44 +0100

    wip

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -12,30 +12,84 @@ return {
     require("mason-nvim-dap").setup({
       ensure_installed = { "js" },
       automatic_installation = true,
-      handlers = {
-        js = function(config)
-          config.adapters = {
-            type = "executable",
-            command = vim.fn.exepath("js-debug-adapter"),
-          }
-          config.configurations = {
-            {
-              type = "node",
-              name = "Debug current file",
-              request = "launch",
-              program = "${file}",
-              -- cwd = vim.fn.getcwd(),
-              -- runtimeArgs = {},
-              -- runtimeExecutable = "",
-              -- sourceMaps = true,
-              -- protocol = "inspector",
-              -- console = "integratedTerminal",
-            },
-          }
-          require("mason-nvim-dap").default_setup(config)
-        end,
-      },
+      -- handlers = {
+      --   js = function(config)
+      --     config.adapters = {
+      --       type = "executable",
+      --       command = vim.fn.exepath("js-debug-adapter"),
+      --     }
+      --     config.configurations123 = {
+      --       {
+      --         type = "node",
+      --         name = "Debug current file",
+      --         request = "launch",
+      --         program = "${file}",
+      --         -- cwd = vim.fn.getcwd(),
+      --         -- runtimeArgs = {},
+      --         -- runtimeExecutable = "",
+      --         -- sourceMaps = true,
+      --         -- protocol = "inspector",
+      --         -- console = "integratedTerminal",
+      --       },
+      --     }
+      --     require("mason-nvim-dap").default_setup(config)
+      --   end,
+      -- },
     })
+    dap.set_log_level("TRACE")
+    -- dap.adapters.node = {
+    --   type = "executable",
+    --   command = "js-debug-adapter",
+    --   args = {},
+    -- }
+    -- dap.adapters.node = {
+    --   type = "executable",
+    --   command = "node",
+    --   args = {
+    --     "/Users/husa/.local/share/nvim/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
+    --   },
+    -- }
+    --
+    dap.adapters.node = {
+      type = "server",
+      host = "localhost",
+      port = "${port}",
+      executable = {
+        command = "node",
+        args = {
+          "/Users/husa/.local/share/nvim/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
+          "${port}",
+        },
+      },
+    }
+    require("dap").adapters["pwa-node"] = {
+      type = "server",
+      host = "localhost",
+      port = "${port}",
+      executable = {
+        command = "node",
+        -- ?? Make sure to update this path to point to your installation
+        args = { vim.fn.exepath("js-debug-adapter"), "${port}" },
+      },
+    }
+
+    -- default configurations
+    require("dap").configurations.javascript = {
+      {
+        type = "node",
+        request = "launch",
+        name = "Launch file nvim",
+        program = "${file}",
+        cwd = "${workspaceFolder}",
+      },
+      {
+        type = "node",
+        request = "attach",
+        name = "attach nvim",
+        program = "${file}",
+        cwd = "${workspaceFolder}",
+      },
+    }
 
     require("nvim-dap-virtual-text").setup()
 
@@ -46,6 +100,22 @@ return {
     -- for _, lang in ipairs(js_filetypes) do
     --   dap.configurations[lang] = js_config
     -- end
+
+    -- custom sign icons and highlights
+    local sign_icons = {
+      DapStopped = { "󰁕 ", "DiagnosticSignWarn", "DapStoppedLine", "DiagnosticSignWarn" },
+      DapBreakpoint = { " ", "DiagnosticSignInfo", nil, "DiagnosticSignInfo" },
+      DapBreakpointCondition = { " ", "DiagnosticSignInfo", nil, "DiagnosticSignInfo" },
+      DapBreakpointRejected = { " ", "DiagnosticSignError" },
+      DapLogPoint = { ".>" },
+    }
+
+    for name, sign in pairs(sign_icons) do
+      vim.fn.sign_define(
+        name,
+        { text = sign[1], texthl = sign[2] or "DiagnosticSignInfo", linehl = sign[3], numhl = sign[4] }
+      )
+    end
 
     -- Keymaps
     local map = function(keys, func, desc)
