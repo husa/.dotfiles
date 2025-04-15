b6824531e905d933206703b6d71facb0d11d4d05
husa <1536549+husa@users.noreply.github.com>
2025-04-15 15:00:07 +0200

    (nvim) add colorizer config

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -2,10 +2,13 @@ return {
   "norcalli/nvim-colorizer.lua",
   -- version = "*", -- no releases
   config = function()
-    require("colorizer").setup({
-      "*",
-      css = { css = true },
-      scss = { css = true },
-    })
+    require("colorizer").setup(
+      {
+        "*",
+        css = { css = true },
+        scss = { css = true },
+      },
+      { css = true } -- enable all CSS color formats
+    )
   end,
 }
