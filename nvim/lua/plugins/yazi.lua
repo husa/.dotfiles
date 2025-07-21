62c7a295f09c096147bbf187ac2c5921a1952939
husa <1536549+husa@users.noreply.github.com>
2025-07-22 00:34:41 +0200

    (nvim) update plugins, switch to different yazi plugin

--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -1,7 +1,7 @@
 return {
-  "DreamMaoMao/yazi.nvim",
+  "mikavilpas/yazi.nvim",
   version = "*",
-  -- event = "VeryLazy",
+  event = "VeryLazy",
   keys = {
     { "<leader>ey", "<cmd>Yazi<cr>", desc = "Open Yazi" },
     -- { "<leader>eY", "<cmd>Yazi cwd<cr>", desc = "Open Yazi(cwd)" },
