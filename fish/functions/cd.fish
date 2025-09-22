a1e67056610ed2faf6d37f81261c1ad64568218b
husa <1536549+husa@users.noreply.github.com>
2025-09-22 13:53:02 +0200

    (fish) add "cd" wrapper to handle "cd ..."

---
+++ fish/functions/cd.fish
@@ -0,0 +1,9 @@
+function cd --wraps cd --description 'Change the current directory' -a path
+    if test (string match -r '^\.{2,}$' $path)
+        set dotcount (string length -- $path)
+        set target_path (string repeat -n (math $dotcount - 1) "../")
+        builtin cd $target_path
+    else
+        builtin cd $argv
+    end
+end
