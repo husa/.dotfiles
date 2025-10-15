da072e4074451a0ecb7d28b0ce3aa514b76c099e
husa <1536549+husa@users.noreply.github.com>
2025-10-15 15:38:44 +0200

    (nvim) remove references to eslint_d

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -5,11 +5,11 @@ return {
     local lint = require("lint")
 
     lint.linters_by_ft = {
-      typescript = { "eslint_d" },
-      javascript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
-      vue = { "eslint_d" },
+      -- typescript = { "eslint_d" },
+      -- javascript = { "eslint_d" },
+      -- javascriptreact = { "eslint_d" },
+      -- typescriptreact = { "eslint_d" },
+      -- vue = { "eslint_d" },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
