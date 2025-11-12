72d0aa457164dd51859c6d963f92c46f6429625e
husa <1536549+husa@users.noreply.github.com>
2025-11-12 15:29:47 +0100

    (nvim) add diffview

---
+++ nvim/lua/plugins/diffview.lua
@@ -0,0 +1,7 @@
+return {
+  "sindrets/diffview.nvim",
+  keys = {
+    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
+    { "<leader>gD", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
+  },
+}
