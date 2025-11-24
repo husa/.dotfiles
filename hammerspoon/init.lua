50e17a7230dc1bda4d89b213ef6ebbe8af37b43d
husa <1536549+husa@users.noreply.github.com>
2025-11-24 14:27:31 +0100

    (hammerspoon) disable window man, whichkey

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -16,97 +16,98 @@ hs.hotkey.bind(hyperKey, "R", function()
 end)
 
 -- hs.loadSpoon("MiroWindowsManager")
-hs.loadSpoon("WhichApp")
-local catpuccinPalette = {
-  rosewater = "#f5e0dc",
-  flamingo = "#f2cdcd",
-  pink = "#f5c2e7",
-  mauve = "#cba6f7",
-  red = "#f38ba8",
-  maroon = "#eba0ac",
-  peach = "#fab387",
-  yellow = "#f9e2af",
-  green = "#a6e3a1",
-  teal = "#94e2d5",
-  sky = "#89dceb",
-  sapphire = "#74c7ec",
-  blue = "#89b4fa",
-  lavender = "#b4befe",
-  text = "#cdd6f4",
-  subtext1 = "#bac2de",
-  subtext0 = "#a6adc8",
-  overlay2 = "#9399b2",
-  overlay1 = "#7f849c",
-  overlay0 = "#6c7086",
-  surface2 = "#585b70",
-  surface1 = "#45475a",
-  surface0 = "#313244",
-  base = "#1e1e2e",
-  mantle = "#181825",
-  crust = "#11111b",
-}
-spoon.WhichApp.apps = {
-  { key = "b", app = "Bitwarden", desc = " Bitwarden", color = { hex = catpuccinPalette.blue } },
-  {
-    key = "c",
-    app = "Google Chrome",
-    desc = " Chrome",
-    color = { hex = catpuccinPalette.yellow },
-  },
 
-  { key = "e", app = "Google Chrome Beta", desc = " Chrome Beta", color = { hex = catpuccinPalette.yellow } },
-  { key = "f", app = "Fork", desc = " Fork", color = { hex = catpuccinPalette.teal } },
-  { key = "m", app = "YouTube Music", desc = " Music", color = { hex = catpuccinPalette.red } },
-  { key = "n", app = "Neovide", desc = " Neovide", color = { hex = catpuccinPalette.green } },
-  {
-    key = "o",
-    app = "Obsidian",
-    desc = "󰠮 Obsidian",
-    color = { hex = catpuccinPalette.mauve },
-  },
-  {
-    key = "o",
-    modifier = "shift",
-    app = "Outlook (PWA)",
-    desc = "󰴢 Outlook",
-    keySymbol = "O",
-    color = { hex = catpuccinPalette.sky },
-  },
-  { key = "s", app = "Slack", desc = " Slack", color = { hex = catpuccinPalette.maroon } },
-  { key = "t", app = "Microsoft Teams", desc = "󰊻 Teams", color = { hex = catpuccinPalette.mauve } },
-  { key = "w", app = "Wezterm", desc = " Wezterm", color = { hex = catpuccinPalette.lavender } },
-  { key = "z", app = "Zen", desc = " Zen Browser", color = { hex = catpuccinPalette.mauve } },
-  {
-    key = "space",
-    app = "Alfred 5",
-    keySymbol = "󱁐",
-    desc = "󰮤 Alfred",
-    color = { hex = catpuccinPalette.pink },
-  },
-  {
-    key = "1",
-    modifier = "shift",
-    app = "Admin By Request",
-    keySymbol = "!",
-    desc = "󱥡 ABR",
-    color = { hex = catpuccinPalette.peach },
-  },
-}
-spoon.WhichApp.options = {
-  columns = 2,
-  dialogWidth = 550,
-  fontName = "Maple Mono NF",
-  fontSize = 18,
-  separator = " -> ",
-  backgroundColor = { hex = catpuccinPalette.crust, alpha = 0.85 },
-  screen = "main",
-  -- hideTimeout = 0,
-}
-
-spoon.WhichApp:bindHotkeys({
-  show = { { "cmd" }, "escape" },
-  hide = { nil, "escape" },
-})
+-- hs.loadSpoon("WhichApp")
+-- local catpuccinPalette = {
+--   rosewater = "#f5e0dc",
+--   flamingo = "#f2cdcd",
+--   pink = "#f5c2e7",
+--   mauve = "#cba6f7",
+--   red = "#f38ba8",
+--   maroon = "#eba0ac",
+--   peach = "#fab387",
+--   yellow = "#f9e2af",
+--   green = "#a6e3a1",
+--   teal = "#94e2d5",
+--   sky = "#89dceb",
+--   sapphire = "#74c7ec",
+--   blue = "#89b4fa",
+--   lavender = "#b4befe",
+--   text = "#cdd6f4",
+--   subtext1 = "#bac2de",
+--   subtext0 = "#a6adc8",
+--   overlay2 = "#9399b2",
+--   overlay1 = "#7f849c",
+--   overlay0 = "#6c7086",
+--   surface2 = "#585b70",
+--   surface1 = "#45475a",
+--   surface0 = "#313244",
+--   base = "#1e1e2e",
+--   mantle = "#181825",
+--   crust = "#11111b",
+-- }
+-- spoon.WhichApp.apps = {
+--   { key = "b", app = "Bitwarden", desc = " Bitwarden", color = { hex = catpuccinPalette.blue } },
+--   {
+--     key = "c",
+--     app = "Google Chrome",
+--     desc = " Chrome",
+--     color = { hex = catpuccinPalette.yellow },
+--   },
+--
+--   { key = "e", app = "Google Chrome Beta", desc = " Chrome Beta", color = { hex = catpuccinPalette.yellow } },
+--   { key = "f", app = "Fork", desc = " Fork", color = { hex = catpuccinPalette.teal } },
+--   { key = "m", app = "YouTube Music", desc = " Music", color = { hex = catpuccinPalette.red } },
+--   { key = "n", app = "Neovide", desc = " Neovide", color = { hex = catpuccinPalette.green } },
+--   {
+--     key = "o",
+--     app = "Obsidian",
+--     desc = "󰠮 Obsidian",
+--     color = { hex = catpuccinPalette.mauve },
+--   },
+--   {
+--     key = "o",
+--     modifier = "shift",
+--     app = "Outlook (PWA)",
+--     desc = "󰴢 Outlook",
+--     keySymbol = "O",
+--     color = { hex = catpuccinPalette.sky },
+--   },
+--   { key = "s", app = "Slack", desc = " Slack", color = { hex = catpuccinPalette.maroon } },
+--   { key = "t", app = "Microsoft Teams", desc = "󰊻 Teams", color = { hex = catpuccinPalette.mauve } },
+--   { key = "w", app = "Wezterm", desc = " Wezterm", color = { hex = catpuccinPalette.lavender } },
+--   { key = "z", app = "Zen", desc = " Zen Browser", color = { hex = catpuccinPalette.mauve } },
+--   {
+--     key = "space",
+--     app = "Alfred 5",
+--     keySymbol = "󱁐",
+--     desc = "󰮤 Alfred",
+--     color = { hex = catpuccinPalette.pink },
+--   },
+--   {
+--     key = "1",
+--     modifier = "shift",
+--     app = "Admin By Request",
+--     keySymbol = "!",
+--     desc = "󱥡 ABR",
+--     color = { hex = catpuccinPalette.peach },
+--   },
+-- }
+-- spoon.WhichApp.options = {
+--   columns = 2,
+--   dialogWidth = 550,
+--   fontName = "Maple Mono NF",
+--   fontSize = 18,
+--   separator = " -> ",
+--   backgroundColor = { hex = catpuccinPalette.crust, alpha = 0.85 },
+--   screen = "main",
+--   -- hideTimeout = 0,
+-- }
+--
+-- spoon.WhichApp:bindHotkeys({
+--   show = { { "cmd" }, "escape" },
+--   hide = { nil, "escape" },
+-- })
 -- window navigation
 -- require("window-navigation")
 
@@ -116,7 +117,7 @@ spoon.WhichApp:bindHotkeys({
 -- require("playground")
 
 -- window manager
-require("window-manager")
+-- require("window-manager")
 
 -- use Option+Tab to cycle through windows of the focused application
 require("optiontab")
