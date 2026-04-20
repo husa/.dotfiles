314515537c6f1cfb7c08835169f594150365b213
husa <1536549+husa@users.noreply.github.com>
2026-04-20 15:29:08 +0200

    (nvim) switch to diffview active folk

--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,5 +1,6 @@
 return {
-  "sindrets/diffview.nvim",
+  -- "sindrets/diffview.nvim",
+  "dlyongemallo/diffview.nvim", -- actively maintained fork of sindrets/diffview.nvim
   keys = {
     { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
     { "<leader>gD", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
