d9a6c4239303c0768ee6954f108e20e8a3605a62
husa <1536549+husa@users.noreply.github.com>
2026-09-01 17:42:19 +0200

    (nvim) format html using prettier

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -32,6 +32,7 @@ return {
       json = { "prettierd", "prettier", stop_after_first = true },
       markdown = { "prettierd", "prettier", stop_after_first = true },
       yaml = { "prettierd", "prettier", stop_after_first = true },
+      html = { "prettierd", "prettier", stop_after_first = true },
 
       css = { "prettier" },
       scss = { "prettier" },
